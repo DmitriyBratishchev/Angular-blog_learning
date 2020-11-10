49808ccaa289d832c84f83e689bded7cd86ab7f6
Dmitriy Bratishchev <dmitriy-1008@yandex.ru>
2020-11-10 13:42:01 +0300

    Angular-blog learning

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBkhA6K2vD1x6WsLKaV2xwoCjW-RcTzBoY',
+  fbDbUrl: 'https://angular-blog-2a178.firebaseio.com'
 };
---
+++ src/environments/interface.ts
@@ -0,0 +1,10 @@
+export interface Environment {
+  apiKey: string,
+  production: boolean,
+  fbDbUrl: string
+}
+
+export interface FbAuthResponse {
+  idToken: string
+  expiresIn: string
+}
